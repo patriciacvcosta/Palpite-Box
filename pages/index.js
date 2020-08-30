995f72cbd60ca1a6ea447f78ce310a4fb4bdfea5
Patricia Canuto Vieira da Costa <patriciacvcosta@gmail.com>
2020-08-29 23:01:14 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'  //Optimizes the page rendering using prefetch. It loads the page before the user clicks the link.
+
+const Index = () => {
+    return (
+        <div>
+            <p className='mt-12 text-center font-bold'>
+                O restaurante X sempre busca por atender melhor seus clientes.<br />
+                Por isso, estamos sempre abertos a ouvir a sua opinião.
+            </p>
+            <div className='text-center my-12'>
+                <Link href='/pesquisa'>
+                    <a className='bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow'>Dar opinião ou sugestão</a>
+                </Link>
+            </div>
+            <p className='my-12 text-center'>
+                Mensagem do desconto. Texto dinâmico vindo da planilha.
+            </p>
+        </div>
+    )
+}
+
+export default Index
---
+++ pages/pesquisa.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Pesquisa = () => {
+    return (
+        <div className='pt-6'>
+            <h1 className='text-2xl text-center font-bold my-4'>Críticas e Sugestões</h1>
+
+            <p className='text-center mb-6'>
+                O restaurante X sempre busca por atender melhor seus clientes.<br />
+                Por isso, estamos sempre abertos a ouvir a sua opinião.
+            </p>
+
+            <div className='w-1/5 mx-auto'>
+                <label className='font-bold'>Seu nome:</label>
+                <input type='text' className='p-4 block shadow bg-blue-100 my-2 rounded' />
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Pesquisa

db55779b2bed38d900e4a9ebd1c56d8f355d7611
Patricia Canuto Vieira da Costa <patriciacvcosta@gmail.com>
2020-09-04 17:44:24 -0400

    Created get-promo API to know if there's a coupon available and what is the promo message.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Link from 'next/link'  //Optimizes the page rendering using prefetch. It loads the page before the user clicks the link.
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const Index = () => {
-    return (
+    const { data, error } = useSWR('/api/get-promo', fetcher)   //useSWR needs an URL from where it will fetch the data + how it will fetch that data
 
-        <p>adicionando uma linha</p>
+    return (
         <div>
             <p className='mt-12 text-center font-bold'>
                 O restaurante X sempre busca por atender melhor seus clientes.<br />
@@ -15,9 +18,12 @@ const Index = () => {
                     <a className='bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow'>Dar opinião ou sugestão</a>
                 </Link>
             </div>
-            <p className='my-12 text-center'>
-                Mensagem do desconto. Texto dinâmico vindo da planilha.
+            {!data && <p>Carregando...</p>}
+            {data && data.showCoupon &&
+                <p className='my-12 text-center'>
+                    {data.message}
             </p>
+            }
         </div>
     )
 }

bddeda99b443bb750a6a77863446d7604a5bb74c
Patricia Canuto Vieira da Costa <patriciacvcosta@gmail.com>
2020-09-05 18:02:27 -0400

    1) Because the private key was not being read when using vercel, it had to be converted to base64 to get rid of any special characters. This conversion was done using an online encoder. 2) Substituted the new encoded base64  private key on the .env.local 3) An utils/base64.js file was created to have a methos that converts the new base64 private key back to ascii base 4) the frombase64 method is called on the get-promo and save APIs, when the SHEET_PRIVATE_KEY is used.

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,19 +1,20 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import moment from 'moment'
+import { fromBase64 } from '../../utils/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-const genCoupon = () =>{
+const genCoupon = () => {
     const code = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCase()
 
-    return code.substr(0,4) + '-' + code.substr(4,4) + '-' + code.substr(8,4)
+    return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
 }
 
 export default async (req, res) => {
     try {
         await doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL,
-            private_key: process.env.SHEET_PRIVATE_KEY
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         })
         await doc.loadInfo()
         const sheet = doc.sheetsByIndex[1]
@@ -23,14 +24,14 @@ export default async (req, res) => {
 
         const configSheet = doc.sheetsByIndex[2]
         await configSheet.loadCells('C3:C4')
-        
+
         const promoMessageCell = configSheet.getCell(3, 2)
         const activatePromoCell = configSheet.getCell(2, 2)
 
         let Coupon = ''
         let Promo = ''
 
-        if(activatePromoCell.value === true){
+        if (activatePromoCell.value === true) {
             Coupon = genCoupon()
             Promo = promoMessageCell.value
         }
@@ -40,7 +41,7 @@ export default async (req, res) => {
             Email: data.Email,
             Whatsapp: data.Whatsapp,
             Score: parseInt(data.Score),
-            'Data Preenchimento' : moment().format('DD/MM/YYYY, HH:mm:ss'),
+            'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm:ss'),
             Coupon,
             Promo
         })
